8afce9fc59c51a660272f6cf0ab3fa8fa99ace86
Jepheto <121671425+Jepheto@users.noreply.github.com>
2025-04-23 03:12:42 +0900

    .

--- main.py
+++ main.py
@@ -61,10 +61,10 @@ async def fetch_user_data(user_id):
                 "user_id": user_id or "정보 없음",
                 "level": str(user_data.get("user_level", "0")),
                 "exp": str(user_data.get("user_exp", 0)),
-                "id_birthday": (time_conversion_system.format_ID_birthday_message(user_data.get("user_date_create")) if user_data.get("user_date_create") else "정보 없음"),
-                "last_login": (time_conversion_system.format_last_login_message(user_data.get("user_date_last_login")) if user_data.get("user_date_last_login") else "정보 없음"),
-                "last_logout": (time_conversion_system.format_last_logout_message(user_data.get("user_date_last_logout")) if user_data.get("user_date_last_logout") else "정보 없음"),
-                "is_online": online_info["status"] if "status" in online_info else False,
+                "id_birthday": time_conversion_system.format_ID_birthday_message(user_data.get("user_date_create")) if user_data.get("user_date_create") else "정보 없음",
+                "last_login": time_conversion_system.format_last_login_message(user_data.get("user_date_last_login")) if user_data.get("user_date_last_login") else "정보 없음",
+                "last_logout": time_conversion_system.format_last_logout_message(user_data.get("user_date_last_logout")) if user_data.get("user_date_last_logout") else "정보 없음",
+                "is_online": online_info.get("status", False),
                 "check_message": online_info.get("message", "정보 없음"),
                 "guild_info": str(guild_data.get("guild_id")) if guild_data and "guild_id" in guild_data else "길드 없음"
                 }
